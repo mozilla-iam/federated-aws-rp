af102baa0442a35670c755c6eb1f21f6dbc759c4
Gene Wood <gene_wood@cementhorizon.com>
2019-11-04 19:35:53 -0800

    Change config objection creation

--- functions/federated_aws_rp/config.py
+++ functions/federated_aws_rp/config.py
@@ -5,31 +5,33 @@ from jinja2 import Template
 
 
 class Config:
-    pass
+    def __init__(self):
+        self.s3_bucket_name = os.getenv('S3_BUCKET_NAME')
+        self.s3_file_path_store = os.getenv(
+            'S3_FILE_PATH_STORE', 'federated-aws-rp-store.json')
+        self.client_id = os.getenv('CLIENT_ID')
+        self.domain_name = os.getenv('DOMAIN_NAME')
+        self.discovery_url = os.getenv('DISCOVERY_URL')
+        self.log_level = os.getenv('LOG_LEVEL', 'INFO')
 
-
-CONFIG = Config()
-CONFIG.s3_bucket_name = os.getenv('S3_BUCKET_NAME')
-CONFIG.s3_file_path_store = os.getenv(
-    'S3_FILE_PATH_STORE', 'federated-aws-rp-store.json')
-CONFIG.client_id = os.getenv('CLIENT_ID')
-CONFIG.domain_name = os.getenv('DOMAIN_NAME')
-CONFIG.discovery_url = os.getenv('DISCOVERY_URL')
-CONFIG.log_level = os.getenv('LOG_LEVEL', 'INFO')
-
-CONFIG.bypass_cache = False
-CONFIG.redirect_uri_path = '/redirect_uri'
-# CONFIG.oidc_scope = 'openid email'
-CONFIG.oidc_scope = 'openid'
-CONFIG.redirect_uri = urllib.parse.urlunparse(
-    ('https', CONFIG.domain_name, CONFIG.redirect_uri_path, '', '', ''))
-CONFIG.cookie_name = 'federated-aws-rp'
-CONFIG.id_token_for_roles_url = 'https://roles-and-aliases.security.mozilla.org/roles'
-CONFIG.default_destionation_url = 'https://console.aws.amazon.com/console/home'
-CONFIG.role_picker_template = Template('''{% block body %}
+        self.bypass_cache = False
+        self.redirect_uri_path = '/redirect_uri'
+        # self.oidc_scope = 'openid email'
+        self.oidc_scope = 'openid'
+        self.redirect_uri = urllib.parse.urlunparse(
+            ('https', self.domain_name, self.redirect_uri_path, '', '', ''))
+        self.cookie_name = 'federated-aws-rp'
+        self.id_token_for_roles_url = (
+            'https://roles-and-aliases.security.mozilla.org/roles')
+        self.default_destionation_url = (
+            'https://console.aws.amazon.com/console/home')
+        self.role_picker_template = Template('''{% block body %}
   <ul>
   {% for role in roles %}
     <li><a href="/pick_role?role_arn={{ role.arn }}">{{ role.name }}</a><br><span style="margin-left: 2em;">Account: {{role.alias}} ({{role.id}})</span></li>
   {% endfor %}
   </ul>
 {% endblock %}''')
+
+
+CONFIG = Config()
